ebf0733ff1a134aac521ea432042c7b24cf53c4d
root <root@localhost.localdomain>
2016-08-21 12:06:43 -0400

    scala test

---
+++ HelloWorld.scala
@@ -0,0 +1,10 @@
+object HelloWorld{
+    var myVar : Int = 10        //变量 
+    var myVar1 = 10
+    val myvar : String = "Foo"  //常量 
+    val myvar1 = "Foo"     //可省略类型, 但必须有初始化的值
+
+    def main(args:Array[String]){
+        println("Hello, World!");
+    }
+}
